35e2c22bc8b8d65936c33edc538f52019b8de83a
Ashish Prajapati <ashisherc@gmail.com>
2023-09-03 13:58:17 +0530

    export aux parser

--- src/parser/alonzo/index.ts
+++ src/parser/alonzo/index.ts
@@ -2,3 +2,4 @@ export { parseHeader } from "./headerParser";
 export { parseBlock } from "./blockParser";
 export { parseTransaction } from "./transactionBodyParser";
 export { parseWitnessMap } from "./witnessesParser";
+export { parseAuxiliaryData } from "./auxiliaryDataParser";
