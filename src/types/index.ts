985ace993b3832c6aa1474860169432746004a81
Ashish Prajapati <ashisherc@gmail.com>
2024-11-08 11:44:39 +0530

    conway types

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,4 @@ export * as AlonzoTypes from "./alonzoTypes";
 export * as ByronTypes from "./byronTypes";
 export * as BabbageTypes from "./babbageTypes";
 export * as OuroborosTypes from "./ouroborosTypes";
+export * as ConwayTypes from "./conwayTypes";
